43a70fc81acb3e9e278d55acd07f86a86bfcd96a
Maxim <maksukh@inbox.ru>
2022-06-11 04:48:37 +0300

    Download file

--- src/main/kotlin/ru/chimchima/alexey/controller/PiratController.kt
+++ src/main/kotlin/ru/chimchima/alexey/controller/PiratController.kt
@@ -3,12 +3,11 @@ package ru.chimchima.alexey.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.chimchima.alexey.service.PiratService
-import javax.servlet.http.HttpServletResponse
 
 @RestController
 class PiratController(
     private val piratService: PiratService
 ) {
-    @GetMapping("/")
-    fun pirat(response: HttpServletResponse) = piratService.getPirat(response)
+    @GetMapping("/kal.mp3")
+    fun pirat() = piratService.getPirat()
 }
--- src/main/kotlin/ru/chimchima/alexey/service/PiratService.kt
+++ src/main/kotlin/ru/chimchima/alexey/service/PiratService.kt
@@ -1,14 +1,20 @@
 package ru.chimchima.alexey.service
 
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.io.File
-import javax.servlet.http.HttpServletResponse
+import java.io.FileInputStream
 
 @Service
 class PiratService {
-    fun getPirat(response: HttpServletResponse) {
-        val kal = File("pirat.mp3").inputStream()
-        response.contentType = "mp3"
-        kal.copyTo(response.outputStream)
+    fun getPirat(): ResponseEntity<InputStreamResource> {
+        val resource = InputStreamResource(FileInputStream("pirat.mp3"))
+
+        return ResponseEntity.ok()
+            .contentLength(File("pirat.mp3").length())
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(resource)
     }
 }
